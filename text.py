98969f2cb7363d11b92be77b4ffeac9c4d770ea2
Guilherme Souza <ogrsouza@gmail.com>
2019-05-27 09:55:43 -0300

    Trace renamed to Dash

--- text.py
+++ text.py
@@ -1,7 +1,7 @@
 
 
 DOT = '1'
-TRACE = '111'
+DASH = '111'
 SPACE_LETTER = '0'
 SPACE_BETWEEN_LETTERS = '000'
 SPACE_BETWEEN_WORDS = '0000000'
@@ -9,52 +9,47 @@ SPACE_BETWEEN_WORDS = '0000000'
 SUPPORTED_CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 
 MORSE_TABLE = {
-    'A': [DOT, TRACE],
-    'B': [TRACE, DOT, DOT, DOT],
-    'C': [TRACE, DOT, TRACE, DOT],
-    'D': [TRACE, DOT, DOT],
+    'A': [DOT, DASH],
+    'B': [DASH, DOT, DOT, DOT],
+    'C': [DASH, DOT, DASH, DOT],
+    'D': [DASH, DOT, DOT],
     'E': [DOT],
-    'F': [DOT, DOT, TRACE, DOT],
-    'G': [TRACE, TRACE, DOT],
+    'F': [DOT, DOT, DASH, DOT],
+    'G': [DASH, DASH, DOT],
     'H': [DOT, DOT, DOT, DOT],
     'I': [DOT, DOT],
-    'J': [DOT, TRACE, TRACE, TRACE],
-    'K': [TRACE, DOT, TRACE],
-    'L': [DOT, TRACE, DOT, DOT],
-    'M': [TRACE, TRACE],
-    'N': [TRACE, DOT],
-    'O': [TRACE, TRACE, TRACE],
-    'P': [DOT, TRACE, TRACE, DOT],
-    'Q': [TRACE, TRACE, DOT, TRACE],
-    'R': [DOT, TRACE, DOT],
+    'J': [DOT, DASH, DASH, DASH],
+    'K': [DASH, DOT, DASH],
+    'L': [DOT, DASH, DOT, DOT],
+    'M': [DASH, DASH],
+    'N': [DASH, DOT],
+    'O': [DASH, DASH, DASH],
+    'P': [DOT, DASH, DASH, DOT],
+    'Q': [DASH, DASH, DOT, DASH],
+    'R': [DOT, DASH, DOT],
     'S': [DOT, DOT, DOT],
-    'T': [TRACE],
-    'U': [DOT, DOT, TRACE],
-    'V': [DOT, DOT, DOT, TRACE],
-    'W': [DOT, TRACE, TRACE],
-    'X': [TRACE, DOT, DOT, TRACE],
-    'Y': [TRACE, DOT, TRACE, TRACE],
-    'Z': [TRACE, TRACE, DOT, DOT],
-    '1': [DOT, TRACE, TRACE, TRACE, TRACE],
-    '2': [DOT, DOT, TRACE, TRACE, TRACE],
-    '3': [DOT, DOT, DOT, TRACE, TRACE],
-    '4': [DOT, DOT, DOT, DOT, TRACE],
+    'T': [DASH],
+    'U': [DOT, DOT, DASH],
+    'V': [DOT, DOT, DOT, DASH],
+    'W': [DOT, DASH, DASH],
+    'X': [DASH, DOT, DOT, DASH],
+    'Y': [DASH, DOT, DASH, DASH],
+    'Z': [DASH, DASH, DOT, DOT],
+    '1': [DOT, DASH, DASH, DASH, DASH],
+    '2': [DOT, DOT, DASH, DASH, DASH],
+    '3': [DOT, DOT, DOT, DASH, DASH],
+    '4': [DOT, DOT, DOT, DOT, DASH],
     '5': [DOT, DOT, DOT, DOT, DOT],
-    '6': [TRACE, DOT, DOT, DOT, DOT],
-    '7': [TRACE, TRACE, DOT, DOT, DOT],
-    '8': [TRACE, TRACE, TRACE, DOT, DOT],
-    '9': [TRACE, TRACE, TRACE, TRACE, DOT],
-    '0': [TRACE, TRACE, TRACE, TRACE, TRACE],
+    '6': [DASH, DOT, DOT, DOT, DOT],
+    '7': [DASH, DASH, DOT, DOT, DOT],
+    '8': [DASH, DASH, DASH, DOT, DOT],
+    '9': [DASH, DASH, DASH, DASH, DOT],
+    '0': [DASH, DASH, DASH, DASH, DASH],
 }
 
 
 def parse_text_to_morse(text):
-    upper_text = text.upper()
-    words = upper_text.split(' ')
-    clean_words = map(lambda word: filter(
-        lambda c: c in SUPPORTED_CHARS, word), words)
-    return SPACE_BETWEEN_WORDS.join(
-        map(lambda word: SPACE_BETWEEN_LETTERS.join(map(lambda l: SPACE_LETTER.join(MORSE_TABLE[l]), word)), clean_words))
+    return SPACE_BETWEEN_WORDS.join(map(lambda word: SPACE_BETWEEN_LETTERS.join(map(lambda l: SPACE_LETTER.join(MORSE_TABLE[l]), word)), map(lambda word: filter(lambda c: c in SUPPORTED_CHARS, word), text.upper().split(' '))))
 
 
 def parse_text_to_audio(text):
